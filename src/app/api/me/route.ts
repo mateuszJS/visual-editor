4686038a9a657f7511bcf5ed7424d04e86376e10
mateuszJS <mate.walendzik@gmail.com>
2025-04-10 04:03:14 +0200

    handle case when there is no user found

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -6,17 +6,17 @@ import getResponseError from '../utils/getResponseError'
 import sanitizeUserData from '../utils/sanitizeUserData'
 
 async function getUser(session: SessionPayload) {
-  const { data, error } = await supabase
-    .from('users')
-    .select()
-    .eq('id', parseInt(session.userId))
-    .single()
+  const { data, error } = await supabase.from('users').select().eq('id', parseInt(session.userId))
 
   if (error) {
     return getResponseError(error.message)
   }
 
-  return NextResponse.json(sanitizeUserData(data))
+  if (data?.length !== 1) {
+    return getResponseError('User not found.', 404)
+  }
+
+  return NextResponse.json(sanitizeUserData(data[0]))
 }
 
 export const GET = withSession(getUser)

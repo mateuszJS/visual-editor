9724eae45db71e8bdb4103f87c6cfa692bd4f647
mateuszJS <mate.walendzik@gmail.com>
2025-04-10 16:10:48 +0200

    install http-server package locally

--- jest.visual.config.ts
+++ jest.visual.config.ts
@@ -3,7 +3,6 @@ import { config as jestConfig } from './jest.config'
 const config = {
   preset: 'jest-puppeteer',
   moduleNameMapper: jestConfig.moduleNameMapper,
-  // not needed for now, but might be useufl in the future if we provide mroe custom setup
   testMatch: ['**/*.visual-test.{js,jsx,ts,tsx}'],
   transform: {
     '^.+\\.tsx?$': [

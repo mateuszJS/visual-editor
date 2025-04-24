fc32c0bc1f06917ba059e74c9d16a525982d6ccc
mateuszJS <mate.walendzik@gmail.com>
2025-04-25 00:53:25 +0200

    disable error and fix posting comment

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'next/navigation'
 
 export default function Project() {
   const params = useParams<{ id: string }>()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const project = useProject(Number(params.id))
 
   return <h1>User&apos;s Projects</h1>

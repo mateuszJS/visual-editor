150a5049e456b540b7cf2bc22aecd695eb7da43a
mateuszJS <mate.walendzik@gmail.com>
2025-06-18 17:16:31 +0200

    more logs!

--- src/hooks/projects/useProject.ts
+++ src/hooks/projects/useProject.ts
@@ -27,6 +27,7 @@ async function updateProject(id: number, project: UpdateProjectPayload) {
         ...project,
       })
     )
+    console.log('update is completed', id, project)
   } catch (err) {
     throw Error(`Failed to update project with id ${id}: ${err}`)
   }
@@ -37,6 +38,7 @@ export default function useProject(id?: number) {
   const projects = useSnapshot(projectsStore)
 
   const { success, error, loading, fetcher } = useFetcher<SanitizedProject>()
+  console.log(success, error, loading, projects.size)
   useEffect(() => {
     if (id && !projectsStore.has(id)) {
       fetcher(`/api/projects/${id}`)

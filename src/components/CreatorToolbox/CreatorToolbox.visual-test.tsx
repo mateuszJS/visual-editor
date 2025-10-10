f3cac75c93dd9a1187a00bd1c84c057b3366eac8
mateuszJS <mate.walendzik@gmail.com>
2025-10-11 01:28:34 +0200

    remove unnecessary test

--- src/components/CreatorToolbox/CreatorToolbox.visual-test.tsx
+++ src/components/CreatorToolbox/CreatorToolbox.visual-test.tsx
@@ -1,9 +1,6 @@
 import visualSetup from 'test/visual-setup'
 
 describe('<CreatorToolbox /> Visual Tests', () => {
-  it('loading', async () => {
-    await visualSetup('creatortoolbox--default&args=initialized%3A!false', __dirname)
-  })
   it('default desktop', async () => {
     await visualSetup('creatortoolbox--default&args=initialized%3A!true', __dirname)
   })

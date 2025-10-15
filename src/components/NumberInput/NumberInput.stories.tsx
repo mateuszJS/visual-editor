6490b74d1ae41f27aaf29b668a102e2ece36c494
mateuszJS <mate.walendzik@gmail.com>
2025-10-15 16:01:35 +0200

    fix import

--- src/components/NumberInput/NumberInput.stories.tsx
+++ src/components/NumberInput/NumberInput.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/nextjs'
 import { fn } from 'storybook/test'
 import NumberInput from './NumberInput'
-import { useArgs } from 'storybook/internal/preview-api'
+import { useArgs } from 'storybook/preview-api'
 
 const meta = {
   component: NumberInput,

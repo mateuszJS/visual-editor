9c6eaa4fa1b7e338a2820ab4c8753431d4b3d967
mateuszJS <mate.walendzik@gmail.com>
2025-10-15 16:10:06 +0200

    update storybook links

--- src/components/CreatorToolbox/CreatorToolbox.visual-test.tsx
+++ src/components/CreatorToolbox/CreatorToolbox.visual-test.tsx
@@ -2,9 +2,11 @@ import visualSetup from 'test/visual-setup'
 
 describe('<CreatorToolbox /> Visual Tests', () => {
   it('default desktop', async () => {
-    await visualSetup('creatortoolbox--default&args=initialized%3A!true', __dirname)
+    await visualSetup('components-creatortoolbox--default&args=initialized%3A!true', __dirname)
   })
   it('default mobile', async () => {
-    await visualSetup('creatortoolbox--default&args=initialized%3A!true', __dirname, { width: 799 })
+    await visualSetup('components-creatortoolbox--default&args=initialized%3A!true', __dirname, {
+      width: 799,
+    })
   })
 })

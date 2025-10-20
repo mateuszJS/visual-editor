3c9e1648f3b90cce886706b6fa992693c6dbb938
mateuszJS <mate.walendzik@gmail.com>
2025-10-20 11:56:42 +0200

    remove deprecated apis

--- _deprecated_api/projects/route.ts
+++
@@ -1,6 +0,0 @@
-import { withSession } from '../wrappers/session'
-import createProject from './createProject'
-import getProjectsList from './getProjectsList'
-
-export const POST = withSession(createProject)
-export const GET = withSession(getProjectsList)

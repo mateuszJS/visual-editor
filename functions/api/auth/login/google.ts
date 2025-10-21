a4ee3214254393fa5cdb8ed8be5da75b226859c2
mateuszJS <mate.walendzik@gmail.com>
2025-10-21 22:30:59 +0200

    fix test, replace process.env with cloudflare:workers env

--- functions/api/auth/login/google.ts
+++ functions/api/auth/login/google.ts
@@ -4,6 +4,7 @@ import getResponseError from '../../../utils/getResponseError'
 import { withCSRFProtection } from '../../../wrappers/csrf'
 import getUserData from '../../../utils/getUserData'
 import withError from '../../../utils/error'
+import { env } from 'cloudflare:workers'
 
 // avoid import from google-auth-library. One of the exports(gcp-metadata -> google-logging-utils)
 // causes claudflare deployments to fail with JS error(Uncaught TypeError: Cannot convert object to primitive value)
@@ -11,7 +12,7 @@ import { OAuth2Client } from 'google-auth-library/build/src/auth/oauth2client'
 
 let client: OAuth2Client | null = null
 
-if (!process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID) {
+if (!env.NEXT_PUBLIC_GOOGLE_CLIENT_ID) {
   throw Error('Env var NEXT_PUBLIC_GOOGLE_CLIENT_ID is missing!')
 }
 
@@ -39,7 +40,7 @@ export const onRequestPost = withCSRFProtection(async (ctx) => {
       }
       const ticket = await client.verifyIdToken({
         idToken: idToken,
-        audience: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+        audience: env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       })
 
       payload = ticket.getPayload()

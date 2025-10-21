b1f41dea65ed39b5036c170c2973b17302954dc1
mateuszJS <mate.walendzik@gmail.com>
2025-10-21 21:47:11 +0200

    use process.env

--- functions/clients/s3.ts
+++ functions/clients/s3.ts
@@ -1,17 +1,16 @@
 import { S3Client } from '@aws-sdk/client-s3'
-import { env } from 'cloudflare:workers'
 
 let client: S3Client | null = null
 
-if (!env.CF_ACCOUNT_ID) {
+if (!process.env.CF_ACCOUNT_ID) {
   throw new Error('CF_ACCOUNT_ID is missing')
 }
 
-if (!env.CF_ACCESS_KEY_ID) {
+if (!process.env.CF_ACCESS_KEY_ID) {
   throw new Error('CF_ACCESS_KEY_ID is missing')
 }
 
-if (!env.CF_R2_SECRET_ACCESS_KEY) {
+if (!process.env.CF_R2_SECRET_ACCESS_KEY) {
   throw new Error('CF_R2_SECRET_ACCESS_KEY is missing')
 }
 
@@ -20,8 +19,8 @@ export default function getS3Client() {
     client = new S3Client({
       endpoint: `https://${env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`,
       credentials: {
-        accessKeyId: env.CF_ACCESS_KEY_ID,
-        secretAccessKey: env.CF_R2_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.CF_ACCESS_KEY_ID,
+        secretAccessKey: process.env.CF_R2_SECRET_ACCESS_KEY,
       },
       region: 'auto',
     })

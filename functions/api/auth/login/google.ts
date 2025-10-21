8bf8a053c0f9022a839732b5b9d9c8243cba1dc4
mateuszJS <mate.walendzik@gmail.com>
2025-10-21 12:51:03 +0200

    test deployment

--- functions/api/auth/login/google.ts
+++ functions/api/auth/login/google.ts
@@ -1,11 +1,12 @@
-import { OAuth2Client, TokenPayload } from 'google-auth-library'
+import type { TokenPayload } from 'google-auth-library'
+// import { OAuth2Client, TokenPayload } from 'google-auth-library'
 import { attachSessionCookie } from '../../../wrappers/session'
 import getResponseError from '../../../utils/getResponseError'
 import { withCSRFProtection } from '../../../wrappers/csrf'
 import getUserData from '../../../utils/getUserData'
 import withError from '../../../utils/error'
 
-let client: OAuth2Client | null = null
+// let client: OAuth2Client | null = null
 
 export const onRequestPost = withCSRFProtection(async (ctx) => {
   const [userData, err] = await withError(async () => {
@@ -17,28 +18,28 @@ export const onRequestPost = withCSRFProtection(async (ctx) => {
 
     let payload: TokenPayload | undefined = undefined
 
-    if (idToken === 'test-account') {
-      payload = {
-        iss: 'https://accounts.google.com',
-        sub: '1234567890',
-        aud: 'the OAuth 2.0 client IDs of your application',
-        iat: 1704067200,
-        exp: 4859740800,
-      }
-    } else {
-      if (!client) {
-        client = new OAuth2Client()
-      }
-      const ticket = await client.verifyIdToken({
-        idToken: idToken,
-        audience: ctx.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      })
-
-      payload = ticket.getPayload()
-      if (!payload) {
-        throw Error('Invalid token payload')
-      }
+    // if (idToken === 'test-account') {
+    payload = {
+      iss: 'https://accounts.google.com',
+      sub: '1234567890',
+      aud: 'the OAuth 2.0 client IDs of your application',
+      iat: 1704067200,
+      exp: 4859740800,
     }
+    // } else {
+    //   if (!client) {
+    //     client = new OAuth2Client()
+    //   }
+    //   const ticket = await client.verifyIdToken({
+    //     idToken: idToken,
+    //     audience: ctx.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    //   })
+
+    //   payload = ticket.getPayload()
+    //   if (!payload) {
+    //     throw Error('Invalid token payload')
+    //   }
+    // }
 
     const userData = await getUserData(ctx.env.db, payload)
     return userData

24a35f7cf1d817cfa15b486df3b8b9e902050ebd
Mateusz Walendzik <mate.walendzik@gmail.com>
2025-10-26 20:20:11 +0100

    deploy storybook to vercel (#94)
    * deploy storybook to vercel
    
    * fix link
    
    * proejct id is not a secret, nor organisation id
    
    * install puppeteer
    
    * trigger change
    
    * revert test
    
    * remove unnecessary env vars
    
    * remove unnecessary github actions

--- jest-puppeteer.config.mjs
+++ jest-puppeteer.config.mjs
@@ -6,7 +6,7 @@
 
 const config = {
   server: {
-    command: 'npm run build-storybook && npx http-server storybook-static -p 6006',
+    command: 'npx http-server storybook-static -p 6006',
     port: 6006,
     launchTimeout: 30000, // 30 seconds to allow Storybook to load
     usedPortAction: 'kill',

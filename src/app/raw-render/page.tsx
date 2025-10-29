2ade07a1ee88488c01717294df2d4c598f6b81a4
mateuszJS <mate.walendzik@gmail.com>
2025-10-29 13:56:55 +0100

    improve raw-render routing

--- src/app/raw-render/page.tsx
+++ src/app/raw-render/page.tsx
@@ -1,10 +1,10 @@
 import useProject from '@/hooks/useProject/useProject'
 import CreatorView from '@/components/CreatorView/CreatorView'
-import { usePathname } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 
 export default function RawRender() {
-  const pathname = usePathname()
-  const id = pathname.split('/').pop()
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
   if (!id) {
     // it should be not possible to have no param on this route
     throw Error('Project id is missing in the URL')

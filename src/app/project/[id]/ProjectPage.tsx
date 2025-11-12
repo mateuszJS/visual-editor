0b2947afdea040988099a6ac21e532be9635f7bd
mateuszJS <mate.walendzik@gmail.com>
2025-11-12 17:26:31 +0100

    remove non-existing component from the page

--- src/app/project/[id]/ProjectPage.tsx
+++ src/app/project/[id]/ProjectPage.tsx
@@ -10,7 +10,7 @@ import BoundsPanel from '@/components/BoundsPanel/BoundsPanel'
 import useIsMobile from '@/hooks/useIsMobile/useIsMobile'
 import { usePathname } from 'next/navigation'
 import { useEffect } from 'react'
-import ShapePropsPanel from '@/components/ShapePropsPanel/ShapePropsPanel'
+// import ShapePropsPanel from '@/components/ShapePropsPanel/ShapePropsPanel'
 
 export default function Project() {
   const isMobile = useIsMobile()
@@ -45,7 +45,7 @@ export default function Project() {
       {!isMobile && (
         <section className={styles.panels}>
           <BoundsPanel />
-          <ShapePropsPanel />
+          {/* <ShapePropsPanel /> */}
         </section>
       )}
     </main>

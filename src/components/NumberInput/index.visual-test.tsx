e84b39baa5a17f401039bc7932cb1ec3b8254876
mateuszJS <mate.walendzik@gmail.com>
2025-11-12 17:37:01 +0100

    provide screenshot test threshold

--- src/components/NumberInput/index.visual-test.tsx
+++ src/components/NumberInput/index.visual-test.tsx
@@ -2,6 +2,6 @@ import visualSetup from 'test/visual-setup'
 
 describe('Number Input', () => {
   it('default', async () => {
-    await visualSetup('components-numberinput--default', __dirname, 0)
+    await visualSetup('components-numberinput--default', __dirname, 100)
   })
 })

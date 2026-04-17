fd52d6e187639d797e053742da522c48e620a393
mateuszJS <mate.walendzik@gmail.com>
2026-04-17 15:07:05 +0200

    add delete endpoint call(endpoitn doesnt exist yet)

--- src/components/ProjectPanel/ProjectPanel.tsx
+++ src/components/ProjectPanel/ProjectPanel.tsx
@@ -2,14 +2,24 @@ import Link from '@/components/Link/Link'
 import VerticalMenu from 'assets/vertical-menu.svg'
 import imagePanelStyles from '@/components/shared/imagePanel.module.css'
 import styles from './ProjectPanel.module.css'
-import Button from '../Button/Button'
 import cn from 'classnames'
+import Popover from '@/components/Popover/Popover'
+import Button from '@/components/Button/Button'
+import useFetcher from '@/hooks/useFetcher/useFetcher'
 
 interface Props {
   id: string
   text: string
 }
 export default function ProjectPanel({ id, text }: Props) {
+  const { loading, fetcher } = useFetcher()
+
+  const deleteProject = () => {
+    fetcher('api/projects/' + id, {
+      method: 'DELETE',
+    })
+  }
+
   return (
     <div
       className={cn(styles.root, imagePanelStyles.imagePanel)}
@@ -20,9 +30,23 @@ export default function ProjectPanel({ id, text }: Props) {
         aria-label={`Go to ${text} project details`}
         className={styles.link}
       ></Link>
-      <Button variant="ghost" className={cn('ml-auto', styles.menu)} iconOnly>
+      {/* <Button variant="ghost" className={cn('ml-auto', styles.menu)} iconOnly>
         <VerticalMenu />
-      </Button>
+      </Button> */}
+
+      <Popover
+        variant="ghost"
+        className={cn('ml-auto', styles.menu)}
+        iconOnly
+        trigger={() => <VerticalMenu />}
+        popoverClassName={styles.popover}
+      >
+        <Button onClick={deleteProject}>
+          Delete Project
+          {loading && 'Loading'}
+        </Button>
+      </Popover>
+
       <p className="mt-auto">{text}</p>
     </div>
   )

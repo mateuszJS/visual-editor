eade49bbba88e93f3bb5d92194d00f5df43cf58a
mateuszJS <mate.walendzik@gmail.com>
2026-05-01 20:08:09 +0200

    fix profile image callback

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,29 +5,23 @@ import useAuthOnly from '@/hooks/useAuthOnly/useAuthOnly'
 import userStore from '@/hooks/userStore/userStore'
 import { useSnapshot } from 'valtio'
 import styles from './profile.module.css'
-import { useState } from 'react'
 
 export default function Profile() {
   useAuthOnly()
   const { user } = useSnapshot(userStore)
-  const [error, setError] = useState(false)
 
   return (
     <main className={styles.main}>
       {user && (
         <>
-          {!error && user.photo ? (
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              className={styles.avatar}
-              alt="user photo"
-              src={user.photo}
-              onError={() => setError(true)}
-            />
-          ) : (
-            <div className={styles.avatar}>{(user.name || user.email)[0]}</div>
-          )}
-          <p className={styles.userName}>{user.name}</p>
+          <div className={styles.avatar}>
+            <span>{(user.name || user.email)[0]}</span>
+            {user.photo && (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img alt="user photo" src={user.photo} />
+            )}
+          </div>
+          <p className="mt-32 mx-4">{user.name}</p>
           <p>{user.email}</p>
           <Logout />
         </>
